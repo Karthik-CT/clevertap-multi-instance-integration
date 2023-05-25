70fe19717c4d88ea5cf586a5a13c2f81d4729125
Karthik Iyer <100284000+Karthik-CT@users.noreply.github.com>
2023-05-25 23:53:13 +0530

    multi instance with push

--- NotificationContentDemo/NotificationViewController.swift
+++ NotificationContentDemo/NotificationViewController.swift
@@ -32,7 +32,7 @@ class NotificationViewController: CTNotificationViewController {
     override func userDidReceive(_ response: UNNotificationResponse?) {
 
         //here in the below code get your accountId and accountToken from the app groups this will help to initiate the selected instance of clevertap dashboard
-        let ctConfig = CleverTapInstanceConfig.init(accountId: "TEST-654-Z9R-646Z", accountToken:  "TEST-2c1-456")
+        let ctConfig = CleverTapInstanceConfig.init(accountId: "account_id", accountToken:  "account_token")
         ctConfig.logLevel = CleverTapLogLevel.debug
         ctConfig.disableIDFV = true
         ctConfig.analyticsOnly = false
