3dc5997c181b0ef7254803a56a7ddcb13ac4f561
Karthik Iyer <karthik.iyer@clevertap.com>
2023-06-02 17:31:06 +0530

    updated ios multi instance push

---
+++ Pods/Target Support Files/Pods-MultiInstanceProject-NotificationContentDemo/Pods-MultiInstanceProject-NotificationContentDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MultiInstanceProject_NotificationContentDemo : NSObject
+@end
+@implementation PodsDummy_Pods_MultiInstanceProject_NotificationContentDemo
+@end
